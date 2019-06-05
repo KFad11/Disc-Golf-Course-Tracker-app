659b7495279b02c1c46ab3c74cf5c9380efda1c6
KFad <kfadigan@yahoo.com>
2019-06-05 11:55:04 -0400

    Get the new course page creating a new course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,15 @@
 class CoursesController < ApplicationController
   def new; end
+
+  def create
+    @course = Course.new(course_params)
+
+    @course.save
+    redirect_to @course
+  end
+
+private
+  def course_params
+    params.require(:course).permit(:name, :location)
+  end
 end
