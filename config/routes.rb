31c4fbbbe3b510a509a2873769e58768ec00ceda
KFad <kfadigan@yahoo.com>
2019-06-06 08:31:27 -0400

    Add Devise views, routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :courses
 
   root 'welcome#index'
+
+  devise_for :disc_golfers, controllers: {
+        sessions: 'disc_golfers/sessions'
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
