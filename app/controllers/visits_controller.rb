0510b72b9066eb4dac1de151c1a26da9efcbe03b
KFad <kfadigan@yahoo.com>
2019-06-07 11:53:02 -0400

    Add CRUD actions to Visits controller

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,9 +1,32 @@
 # frozen_string_literal: true
 
 class VisitsController < ApplicationController
-  def new; end
+  def new
+    @visit = Visit.new
+  end
 
-  def create; end
+  def create
+    @course = Course.find(params[:course_id])
+    @visit = @course.visits.create(visit_params)
+    @visit.save
+    redirect_to course_visits_path(@visit)
+  end
 
-  def index; end
+  def index
+    # binding.pry
+    @visits = Visit.all
+  end
+
+  def destroy
+    @course = Course.find(params[:course_id])
+    @visit = @course.visits.find(params[:id])
+    @visit.destroy
+    redirect_to course_visits_path(@course)
+  end
+
+private
+
+  def visit_params
+    params.require(:visit).permit(:day_visited)
+  end
 end
