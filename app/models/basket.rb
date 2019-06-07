756c060da9f8776bf81fc6e79a09281fd8d75b9d
KFad <kfadigan@yahoo.com>
2019-06-07 11:53:43 -0400

    Add validation to the baskets so no basket # is 0

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -5,4 +5,11 @@ class Basket < ApplicationRecord
   validates :basket_number, presence: true
   validates :par, presence: true
   # validates :basket_number, uniqueness: true
+  validate :non_zero
+
+  def non_zero
+    if basket_number.zero?
+      errors.add(:basket_number, "Field can't be zero")
+    end
+  end
 end
