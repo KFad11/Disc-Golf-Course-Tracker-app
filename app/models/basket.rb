1d5ee4d958ec7479feeb10ab83a31c596a7be24f
KFad <kfadigan@yahoo.com>
2019-06-10 11:07:54 -0400

    add a custom validation for basket par

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -2,9 +2,8 @@
 
 class Basket < ApplicationRecord
   belongs_to :course, required: nil
-  validates :basket_number, presence: true
-  validates :par, presence: true
   validate :non_zero_and_negative
+  validate :par_check
   validates :basket_number,
             uniqueness: { scope: :course_id,
                           message: "A basket number must be unique." }
@@ -16,7 +15,12 @@ class Basket < ApplicationRecord
 private
 
   def non_zero_and_negative
-    errors.add(:basket_number, "Field can't be zero or negative") \
-    if basket_number.nil? || basket_number <= 0 || par.nil? || par <= 0
+    errors.add(:basket_number, "field can't be zero or negative") \
+    if basket_number.nil? || basket_number <= 0
+  end
+
+  def par_check
+    errors.add(:par, "field can't be zero or negative") \
+    if par.nil? || par <= 0
   end
 end
