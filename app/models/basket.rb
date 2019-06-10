acce3397a54de583bc5b9235c331d0779796f586
KFad <kfadigan@yahoo.com>
2019-06-10 09:17:29 -0400

    Refactor basket validation to account for par as well

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -5,18 +5,18 @@ class Basket < ApplicationRecord
   validates :basket_number, presence: true
   validates :par, presence: true
   validate :non_zero_and_negative
-  validates :basket_number, uniqueness: { scope: :course_id,
-    message: "A basket number must be unique." }
+  validates :basket_number,
+            uniqueness: { scope: :course_id,
+                          message: "A basket number must be unique." }
   scope :by_number, -> { order(:basket_number) }
-  # def self.by_number
-  #   order(:basket_number)
-  # end
-
+# def self.by_number
+#  order(:basket_number)
+# end
 
 private
 
   def non_zero_and_negative
     errors.add(:basket_number, "Field can't be zero or negative") \
-    if basket_number.nil? || basket_number <= 0
+    if basket_number.nil? || basket_number <= 0 || par.nil? || par <= 0
   end
 end
