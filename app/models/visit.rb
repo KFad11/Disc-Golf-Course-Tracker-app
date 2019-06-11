6c281facc64c9b7c6ad602c38c760edc1bc1588f
KFad <kfadigan@yahoo.com>
2019-06-11 10:09:32 -0400

    Add validation to the visit to prevent future or no date at creation

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,5 +3,13 @@
 class Visit < ApplicationRecord
   belongs_to :disc_golfer, required: nil
   belongs_to :course, required: nil
-  validates :day_visited, presence: true
+  validate :visit_date_cannot_be_in_the_future
+end
+
+private
+
+def visit_date_cannot_be_in_the_future
+  return if day_visited.present? && day_visited <= Date.today
+
+  errors.add(:day_visited, "can't empty or be in the future")
 end
