1d9f74dacae7acbb5e40b4c483178eb5c308b9cf
KFad <kfadigan@yahoo.com>
2019-06-11 10:08:56 -0400

    Reword validation for course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
-  validates :name, presence: true
-  validates :location, presence: true
+  validates_presence_of :name
+  validates_presence_of :location
   has_many :baskets
   has_many :visits
   has_many :disc_golfers, through: :visits
