c4ab52df2f59181666cb3aa2ab28c6b6347eb886
KFad <kfadigan@yahoo.com>
2019-07-11 16:06:30 -0400

    Load AJAX call in the DOM with JSON data

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,16 +1,17 @@
 $(function(){
-  $(".new_course").on("submit", function(e) {
-    e.preventDefault();
+  $(".new_course").on("submit", function(event) {
+    event.preventDefault();
     $.ajax({
       type: "POST",
       url: this.action,
       data: $(this).serialize(),
       dataType: "JSON"
     }).done(function(response) {
-      console.log(response)
-      const newCourse = new Course(response.name, response.location)
+      const link = this.url + "/" + response.id
+      const newCourse = new Course(response.name, response.location, link)
       const $courseList = $("div.courses ol")
       $courseList.append(newCourse.courseEl())
+      window.location.reload();
     })
   })
 })
