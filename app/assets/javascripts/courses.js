36f24b425d80912128e046554f2cf03ded736cac
KFad <kfadigan@yahoo.com>
2019-07-12 09:31:37 -0400

    Build AJAX call for visits

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,4 +1,16 @@
 $(function(){
+  $("a.load_visits").on("click", function(event){
+    $.ajax({
+      method: "GET",
+      url: this.href
+    }).done(function(response) {
+      $("div.visits").html(response)
+    });
+    event.preventDefault();
+  })
+})
+
+$(function(){
   $(".new_course").on("submit", function(event) {
     event.preventDefault();
     $.ajax({
@@ -11,8 +23,9 @@ $(function(){
       const newCourse = new Course(response.name, response.location, link)
       const $courseList = $("div.courses ol")
       $courseList.append(newCourse.courseEl())
-      window.location.reload();
+      // reactive button
+      // How?
+      // google for jQuery or vanilla JS ways to do it. It's likely one line of code.
     })
   })
 })
-
