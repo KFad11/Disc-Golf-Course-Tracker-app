ac45432adbb32c6c834bdfe4e4ce71eed47626bb
KFad <kfadigan@yahoo.com>
2019-07-15 12:08:44 -0400

    Omit constructor lines not needed

--- app/assets/javascripts/basket.js
+++ app/assets/javascripts/basket.js
@@ -1,17 +1,13 @@
 'use strict'
 
 class Basket {
-  constructor(basket_number, par) {
-    this.basket_number = basket_number
+  constructor(par) {
     this.par = par
   }
 
   basketEl() {
     return `
-      <li>
-        Basket: ${this.basket_number}</br>
         Par: ${this.par}
-      </li>
       <br>
     `
   }
