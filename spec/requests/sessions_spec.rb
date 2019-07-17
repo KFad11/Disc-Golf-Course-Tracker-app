1c28d052da64a6b0e8c73b33f32f772eb3a52f81
KFad <kfadigan@yahoo.com>
2019-07-17 08:11:25 -0400

    Change the session request spec to test getting to the landing page

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,14 +1,7 @@
 require "rails_helper"
 RSpec.describe "Sessions" do
-  it "signs disc golfer in and out" do
-    disc_golfer = create(:disc_golfer)
-
-    sign_in disc_golfer
+  it "root path renders welcome index" do
     get root_path
-    expect(response).to render_template(:index)
-
-    sign_out disc_golfer
-    get root_path
-    expect(response).not_to render_template(:show)
+    expect(response).to render_template(:'welcome/index')
   end
 end
