7fee40c15cdd775fa3d29fec7217bee42c45ac14
KFad <kfadigan@yahoo.com>
2019-07-17 10:18:59 -0400

    Add the par button to the Course element

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -14,6 +14,10 @@ class Course {
         <a href="${this.link}">${this.name}</a>
         <br>
         Location: ${this.location}
+        <br>
+        <span id="par_button">
+          <a class="long_par" href="/courses/${this.id}/longest_par"><button type="button">Longest Par on the course</button></a>
+        </span>
       </li>
     `
   }
